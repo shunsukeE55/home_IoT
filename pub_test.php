337fb9a60897c1f53e2672df523825344abf5f8d
shunsukeE55 <shunsuke.e55@gmail.com>
2017-07-04 00:23:38 +0900

    up
    up

---
+++ pub_test.php
@@ -0,0 +1,19 @@
+<?php
+# MQTT設定
+require( "./phpMQTT.php" );
+
+$mqtt_host = "m11.cloudmqtt.com"; # MQTT ブローカー
+$mqtt_port = 14292; # MQTT ポート番号
+$mqtt_username = "pi";
+$mqtt_password = "1uYBQTs9QnRf";
+
+$mqtt_topic = "homeiot/thermo";
+$mqtt_message = '{"thermomerter"}'; # パブリッシュするメッセージ
+
+# MQTT PUBLISH
+$mqtt = new phpMQTT( $mqtt_host, $mqtt_port, $mqtt_clientid );
+if( $mqtt->connect(true,NULL,$mqtt_username,$mqtt_password) ){
+  $mqtt->publish( $mqtt_topic, $mqtt_message, 0 );
+  $mqtt->close();
+}
+?>
