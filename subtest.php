ec18f2785703008325838a14b28b4f72503e0cc4
shunsukeE55 <shunsuke.e55@gmail.com>
2017-07-06 21:39:22 +0900

    a
    a

--- subtest.php
+++ subtest.php
@@ -2,34 +2,30 @@
 
 require("./phpMQTT.php");
 
-$mqtt_host = "m11.cloudmqtt.com"; # MQTT ブローカー
-$mqtt_port = 14292; # MQTT ポート番号
+$host = "m11.cloudmqtt.com"; # MQTT ブローカー
+$port = 14292; # MQTT ポート番号
 $mqtt_clientid = 'mqtt'.date(YmdHis);
-$mqtt_username = "pi";
-$mqtt_password = "1uYBQTs9QnRf";
+$username = "pi";
+$password = "1uYBQTs9QnRf";
 
-$mqtt_topic = "homeiot/thermo";
+$topic = "homeiot/thermo";
 
-$mqtt = new phpMQTT( $mqtt_host, $mqtt_port, $mqtt_clientid );
+$mqtt = new phpMQTT($host, $port, "ClientID".rand());
 
-if(!$mqtt->connect()){
-	exit(1);
+if(!$mqtt->connect(true,NULL,$username,$password)){
+  exit(1);
 }
 
-$topics[$mqtt_topic] = array("qos"=>0, "function"=>"procmsg");
+//currently subscribed topics
+$topics['topic'] = array("qos"=>0, "function"=>"procmsg");
 $mqtt->subscribe($topics,0);
 
 while($mqtt->proc()){
-		
 }
-
 
 $mqtt->close();
-
-function procmsg($mqtt_topic,$msg){
-		echo "Msg Recieved: ".date("r")."\nTopic:{$mqtt_topic}\n$msg\n";
+function procmsg($topic,$msg){
+  echo "Msg Recieved: $msg";
 }
-	
-
 
 ?>
