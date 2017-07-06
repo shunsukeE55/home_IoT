9a970c75bc063608adb6a76d8ac25435c29f7a99
shunsukeE55 <shunsuke.e55@gmail.com>
2017-07-07 02:00:40 +0900

    a2
    a2

--- subtest.php
+++ subtest.php
@@ -10,13 +10,13 @@ require("./phpMQTT.php");
 
 $host = "m11.cloudmqtt.com"; # MQTT ブローカー
 $port = 14292; # MQTT ポート番号
-$mqtt_clientid = 'mqtt'.date(YmdHis);
+$mqtt_clientid = 'mqtt';
 $username = "pi";
 $password = "1uYBQTs9QnRf";
 
 $topic = "homeiot/thermo";
 
-$mqtt = new phpMQTT($host, $port, "ClientID".rand());
+$mqtt = new phpMQTT($host, $port, $mqtt_clientid);
 
 if(!$mqtt->connect(true,NULL,$username,$password)){
   echo '<p>not connect</p>';
@@ -24,12 +24,13 @@ if(!$mqtt->connect(true,NULL,$username,$password)){
 }
 
 //currently subscribed topics
-$topics['topic'] = array("qos"=>0, "function"=>"procmsg");
+$topics[$topic] = array("qos"=>0, "function"=>"procmsg");
 $mqtt->subscribe($topics,0);
 
 $execount=0;
 while($mqtt->proc(true)){
     if($execount == 15){
+        
         break;
     }
     sleep(1);
